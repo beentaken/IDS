686ab28ac0d2cd1a25b06e240687b8242bfcc122
Thant Lynn Han <tlhan@icloud.com>
2015-11-10 17:48:36 +0800

    Start of project

---
+++ IDS/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  IDS
+//
+//  Created by Admin on 11/10/15.
+//  Copyright © 2015 Personal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
